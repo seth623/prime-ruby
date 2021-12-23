4c9fcda00715b1c36ec03d282a3618063c14cd54
seth <seth623@gmail.com>
2021-12-22 17:12:16 -0700

    Completed Assignment

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+
+    range = (2...integer).to_a
+    
+    if integer == 1 || integer <= 0
+        return false
+    else
+        range.all?{ |divisor| integer % divisor != 0 }
+    end    
+
+end 
